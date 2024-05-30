45977487005691e8917026b6cbb34028c5ab7ed9
Naftali MacBook 14 <naftaliko@gmail.com>
2024-05-30 13:33:43 +0900

    Update code. - Add more implementation of library. - Find solution for can change text and working "See more" and "See Less" - Add button for update text, for check if code working well

--- app/src/main/java/com/nk/textviewseemoretemp/MainActivity.java
+++ app/src/main/java/com/nk/textviewseemoretemp/MainActivity.java
@@ -3,14 +3,22 @@ package com.nk.textviewseemoretemp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.colormoon.readmoretextview.ReadMoreTextView;
-//import com.skyhope.showmoretextview.ShowMoreTextView;
+
+/**
+ * For work well "See More", must be default long text in xml, other not gonna hide text and show "See more"
+ */
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView, text2View;
     private ReadMoreTextView readMoreTextView;
-//    private ShowMoreTextView showMoreTextView;
+    private Button updateTextBtn;
+    private com.borjabravo.readmoretextview.ReadMoreTextView readMoreTextView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +30,26 @@ public class MainActivity extends AppCompatActivity {
         readMoreTextView.setExpandedText(getString(R.string.see_less));
         readMoreTextView.setCollapsedTextColor(R.color.magenta);
         readMoreTextView.setExpandedTextColor(R.color.red);
-        readMoreTextView.setTrimLines(5);
+        readMoreTextView.setTrimLines(2);
 
-//        showMoreTextView.setShowingLine(5);
-//        showMoreTextView.addShowMoreText(getString(R.string.see_more));
-//        showMoreTextView.addShowLessText(getString(R.string.see_less));
+        updateTextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                readMoreTextView.setText("Updated " + getString(R.string.long_text));
+                readMoreTextView.setTrimLength(20); //this code need for close long text, other without this text will be opened and without "See more"
+
+
+                readMoreTextView2.setText("Updated " + getString(R.string.long_text));
+                readMoreTextView2.setTrimLength(20); //this code need for close long text, other without this text will be opened and without "See more"
+            }
+        });
     }
 
     private void setReferences() {
+        textView = findViewById(R.id.text_view);
+        text2View = findViewById(R.id.text2_view);
         readMoreTextView = findViewById(R.id.readMoreTextView);
-//        showMoreTextView = findViewById(R.id.showMoreTextView);
+        updateTextBtn = findViewById(R.id.updateTextBtn);
+        readMoreTextView2 = findViewById(R.id.read_more_text_view);
     }
 }
